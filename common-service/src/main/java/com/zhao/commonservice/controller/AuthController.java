1bb7b58ffaa2f3259bbaf981f65e058dd0772809
zhao <1036978689@qq.com>
2021-08-23 11:57:48 +0800

    init

---
+++ common-service/src/main/java/com/zhao/commonservice/controller/AuthController.java
@@ -0,0 +1,15 @@
+package com.zhao.commonservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: AuthController
+ * @Description: TODO
+ * @Author: zhaolianqi
+ * @Date: 2021/8/23 11:41
+ * @Version: v1.0
+ */
+@RestController
+public class AuthController {
+    
+}

be239bb387507552300810f782493c7034111719
zhao <1036978689@qq.com>
2021-08-24 15:13:43 +0800

    auto refresh config

--- common-service/src/main/java/com/zhao/commonservice/controller/AuthController.java
+++ common-service/src/main/java/com/zhao/commonservice/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.zhao.commonservice.controller;
 
 import com.zhao.common.respvo.BaseResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,12 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date: 2021/8/23 11:41
  * @Version: v1.0
  */
+@RefreshScope
 @RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    @Value(value = "${counter:0}")
+    private Integer counter;
 
     @GetMapping("/test")
     public BaseResponse<String> test(){
-        return BaseResponse.SUCCESS("success");
+        return BaseResponse.SUCCESS("counter: " + counter);
     }
 
 }

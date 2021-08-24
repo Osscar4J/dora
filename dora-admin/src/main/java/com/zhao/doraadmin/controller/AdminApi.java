b13dbe2edb6ce2280adff6c49773fac3733fc5cc
zhao <1036978689@qq.com>
2021-08-24 16:40:59 +0800

    add admin

---
+++ dora-admin/src/main/java/com/zhao/doraadmin/controller/AdminApi.java
@@ -0,0 +1,35 @@
+package com.zhao.doraadmin.controller;
+
+import com.zhao.common.respvo.BaseResponse;
+import com.zhao.commonservice.annotation.CommonPath;
+import com.zhao.commonservice.annotation.LoginRequired;
+import com.zhao.commonservice.utils.CommonUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @ClassName: AdminApi
+ * @Author: zhaolianqi
+ * @Date: 2021/8/24 16:33
+ * @Version: v1.0
+ */
+@LoginRequired
+@RestController
+@RequestMapping("/api/admin")
+public class AdminApi {
+
+    @GetMapping
+    public BaseResponse<String> test(){
+        return BaseResponse.SUCCESS();
+    }
+
+    @CommonPath
+    @GetMapping("/ip")
+    public BaseResponse<String> getIp(HttpServletRequest request){
+        return BaseResponse.SUCCESS(CommonUtils.getIpAddr(request));
+    }
+
+}

672405e2c7976ecb4caff6f66c2e57027604aee5
zhao <1036978689@qq.com>
2021-08-25 11:28:05 +0800

    add zuul

--- common-service/src/main/java/com/zhao/commonservice/utils/CommonUtils.java
+++ common-service/src/main/java/com/zhao/commonservice/utils/CommonUtils.java
@@ -49,18 +49,23 @@ public class CommonUtils {
         if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
             ipAddress = request.getHeader("WL-Proxy-Client-IP");
         }
-        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+        if (ipAddress.equals("127.0.0.1") || ipAddress.equals("0:0:0:0:0:0:0:1")){
             ipAddress = request.getRemoteAddr();
-            if (ipAddress.equals("127.0.0.1") || ipAddress.equals("0:0:0:0:0:0:0:1")) {
-                // 根据网卡取本机配置的IP
-                InetAddress inet = null;
-                try {
-                    inet = InetAddress.getLocalHost();
-                } catch (UnknownHostException e) {
-                    e.printStackTrace();
-                }
-                ipAddress = inet.getHostAddress();
+        }
+        if (ipAddress == null
+                || ipAddress.length() == 0
+                || "unknown".equalsIgnoreCase(ipAddress)
+                || ipAddress.equals("127.0.0.1")
+                || ipAddress.equals("0:0:0:0:0:0:0:1")
+        ) {
+            // 根据网卡取本机配置的IP
+            InetAddress inet = null;
+            try {
+                inet = InetAddress.getLocalHost();
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
             }
+            ipAddress = inet.getHostAddress();
         }
         // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
         if (ipAddress != null && ipAddress.length() > 15) { // "***.***.***.***".length()
